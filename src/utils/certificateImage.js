2694443f35aa3022d286156503dbd7b84af1b065
Aniruth R <aniruth_r@apple.com>
2026-02-26 19:21:07 -0500

    Layout and Data update

--- src/utils/certificateImage.js
+++ src/utils/certificateImage.js
@@ -2,7 +2,7 @@ export const certificatesImage = (certificateName) => {
     const certificateID = certificateName.toLowerCase();
     switch (certificateID) {
         case 'react':
-            return 'https://media.licdn.com/dms/image/v2/D560BAQEAi2tXX6yTVg/company-logo_100_100/B56ZWvKUEqHsAc-/0/1742400471801/hkust_logo?e=1763596800&v=beta&t=fOVLSYX-UMP2wnafFgWEQiVpeIfl8SS548Pgd7dVcXY'
+            return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJkGqpxlp3xYy5UN0RbKeGxyGifJ1vC2l6TA&s'
         case 'aws1':
             return 'https://media.licdn.com/dms/image/v2/D4E0BAQFqdm1TZ-RZKQ/company-logo_200_200/B4EZgOay6gHEAQ-/0/1752588562343/amazon_web_services_logo?e=1763596800&v=beta&t=dEpWPcQ6-Qy_eNyehtgc8KkODdGU19mVuHQU3-jGDNs'
         case 'aws':
